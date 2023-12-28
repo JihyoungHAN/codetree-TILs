3830076b2009c170e0795e00dd8daba6edd50387
JihyoungHAN <gkswlgud4343@hufs.ac.kr>
2023-12-27 17:42:17 -0800

    Title: 지그재그로 이동하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231228/지그재그로 이동하기/moving-with-zigzag.py
+++ 231228/지그재그로 이동하기/moving-with-zigzag.py
@@ -1,21 +1,25 @@
 A, B = map(int, input().split())
 
-num = 1
-time = 1
-distance = 0
-result = 0
-preResult = A
-while result < B:
-    if time%2 == 1:  
-        result = A + num
-    else:            
-        result = A - num
-    num *= 2
-    distance += abs(preResult - result)
-    if result > B: 
-        distance -= (result - B)
-    preResult = result
-    time += 1
+num = 1 #이동할 거리
+time = 1 #더할지 뺄지 결정요소 
+distance = 0 #전체 간 거리
+result = A #B랑 비교할 값 
+
+while result != B:
+    if time%2 == 1: # 더하기 
+        for i in range(abs(result-(A+num))):
+            result += 1
+            distance += 1
+            if result == B: 
+                break;       
+    else:           # 빼기 
+        for i in range(abs(result-(A-num))):
+            result -= 1 
+            distance += 1
+            if result == B: 
+                break; 
+    num += 1
+    time += 1 
     
 
 print(distance)
