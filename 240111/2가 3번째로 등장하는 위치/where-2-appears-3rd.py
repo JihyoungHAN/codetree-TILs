800ed4fc5be26b7c5aee30d25db78182e8c817aa
JihyoungHAN <gkswlgud4343@hufs.ac.kr>
2024-01-10 14:19:27 -0800

    Title: 2가 3번째로 등장하는 위치, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240111/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,10 @@
+num = int(input())
+arr = list(map(int, input().split()))
+cnt = 0
+
+for i in range(num):
+    if arr[i] == 2:
+        cnt += 1
+    if cnt == 3:
+        print(i+1)
+        break
