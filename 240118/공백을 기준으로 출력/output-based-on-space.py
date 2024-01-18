c8acd74d52faa8e0e2974a3ce6f433a1ebc60810
JihyoungHAN <gkswlgud4343@hufs.ac.kr>
2024-01-17 21:46:44 -0800

    Title: 공백을 기준으로 출력, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240118/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,10 @@
+firstArr = input()
+secondArr = input()
+
+for i in range(len(firstArr)):
+    if firstArr[i] != " ":
+        print(firstArr[i], end="")
+
+for i in range(len(secondArr)):
+    if secondArr[i] != " ":
+        print(secondArr[i], end="")
