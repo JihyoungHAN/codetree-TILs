07aaf9d2df66cfbedb8a090fe813310cacbf27c1
JihyoungHAN <gkswlgud4343@hufs.ac.kr>
2024-01-17 22:34:51 -0800

    Title: 문자열 범위 출력하기 2, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 240118/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -5,7 +5,8 @@ if string[-1] == " ":
 length = len(string)
 
 if length < n:
-    print(string)
+    for i in range(length-1, -1, -1):
+        print(string[i], end="")
 else: 
     for i in range(length-1, length-n-1, -1):
         print(string[i], end="")
